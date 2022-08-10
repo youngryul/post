2e67d925643b1d751966bdac24e915247a63e527
PNS212624\Hansol <yrlee00@hansol.com>
2022-08-10 15:32:25 +0900

    8/10 commit

---
+++ src/main/java/com/example/post/controller/PostApiController.java
@@ -0,0 +1,22 @@
+package com.example.post.controller;
+
+
+import com.example.post.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class PostApiController {
+
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestDto){
+
+        System.out.println(requestDto);
+
+    }
+}
